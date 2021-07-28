5cfc27c844ce895a9f212b8de17cc9df739e1a27
Mohammad Areeb Siddiqui <mohammadareebs@gmail.com>
2021-07-28 15:07:42 +0500

    Remove pkg installation from config and add to docker file

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,17 @@ FROM amazoncorretto:16
 
 WORKDIR /root
 
-RUN yum install -y git && git clone https://github.com/mareebsiddiqui/vuln-regex-detector.git
+RUN yum install -y git zip unzip make wget ocaml maven && git clone https://github.com/mareebsiddiqui/vuln-regex-detector.git
 
 WORKDIR /root/vuln-regex-detector/
 
 ENV VULN_REGEX_DETECTOR_ROOT /root/vuln-regex-detector/
+ENV SKIP_PKG_DEPS 1
 
 RUN ./configure
 
-RUN yum install -y "perl(JSON::PP)"
+RUN yum install -y "perl(JSON::PP) perl(HTTP::Daemon)"
+
+EXPOSE 4444
+
+RUN perl server.pl
--- server.pl
+++ server.pl
@@ -12,13 +12,13 @@ print "Listening: <URL:", $d->url, ">\n";
 
 while (my $c = $d->accept) {
     while (my $r = $c->get_request) {
-    if ($r->method eq 'GET' and $r->uri->path eq "/xyzzy") {
-        $r = HTTP::Response->parse("working");
-        $c->send_response($r);
-    }
-    else {
-        $c->send_error(RC_FORBIDDEN)
-    }
+        if ($r->method eq 'GET' and $r->uri->path eq "/xyzzy") {
+            $r = HTTP::Response->parse("working");
+            $c->send_response($r);
+        }
+        else {
+            $c->send_error(RC_FORBIDDEN)
+        }
     }
     $c->close;
     undef($c);
